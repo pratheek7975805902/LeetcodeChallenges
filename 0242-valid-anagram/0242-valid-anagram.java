0f57065c62e9c1b2b439560d40ebc231c2639617
Pratheek A S <017pratheek@gmail.com>
2026-07-26 22:22:33 +0530

    Time: 3 ms (93.55%), Space: 46.4 MB (42.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,7 +4,14 @@ class Solution {
         char[] b=t.toCharArray();
         Arrays.sort(a);
         Arrays.sort(b);
-        return(Arrays.equals(a,b));
-        
+        if(a.length!=b.length){
+            return false;
+        }
+        for(int i=0;i<=a.length-1;i++){
+            if(a[i]!=b[i]){
+                return false;
+            }
+        }
+        return true;
     }
 }
